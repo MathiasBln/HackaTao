96c3010ce8125e3d748a4b95be0767348eea2004
LucasThvn <thauvinlucas@gmail.com>
2019-12-19 14:37:12 +0100

    partie bot v1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -33,23 +33,46 @@ class HomeController extends AbstractController
         if ($AQI <= 50) {
             $color = '#019865';
             $index = 'Bonne';
+            $pollutionMessage = "Niveau de pollution faible ;)";
         } elseif (50 < $AQI && $AQI <= 100) {
             $color = '#FCDF30';
             $index = 'Modérée';
+            $pollutionMessage = "Niveau de pollution modérée :)";
         } elseif (100 < $AQI && $AQI <= 150) {
             $color = '#F89839';
             $index = 'élevée';
+            $pollutionMessage = "Niveau de pollution élevée !";
         } else {
             $color = '#CE1F37';
             $index = 'Dangereuse';
+            $pollutionMessage = "Niveau de pollution trop élevée !";
         }
 
 
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://api.meteo-concept.com/api/forecast/nextHours/?token=e68c1dac48cfc3ea6e33883169ca4752d32db7cff364fee7c292a3c0173ed023&insee=45234');
+        $content = $response->toArray();
+        $probaPluie = $content['forecast'][0]['probarain'];
+
+        if ($probaPluie < 50) {
+            $meteoMessage = "Il fait beau ;))";
+        } else {
+            $meteoMessage = "Il va pleuvoir :((";
+        }
+
+        if ($probaPluie < 50 && ($index == 'Bonne' || $index == 'Modérée')) {
+            $recommendation = 'Privilégies le velo ou la marche !';
+        } else {
+            $recommendation = 'Privilégies le bus ou le tram !';
+        }
 
         return $this->render('home/index.html.twig', [
             "AQI" => $AQI,
             "color" => $color,
             "index" => $index,
+            "pollutionMessage" => $pollutionMessage,
+            "meteoMessage" => $meteoMessage,
+            "recommendation" => $recommendation,
         ]);
     }
 }
