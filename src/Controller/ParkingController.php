e1c073c77231ce5f88513b5eeacb9d2cf2a1b096
LucasThvn <thauvinlucas@gmail.com>
2019-12-19 19:06:51 +0100

    fix api tokens

--- src/Controller/ParkingController.php
+++ src/Controller/ParkingController.php
@@ -14,7 +14,7 @@ class ParkingController extends AbstractController
     public function index()
     {
         $client = HttpClient::create();
-        $response = $client->request('GET', 'https://data.orleans-metropole.fr/api/records/1.0/search/?dataset=mobilite-places-disponibles-parkings-en-temps-reel&rows=24');
+        $response = $client->request('GET', 'https://data.orleans-metropole.fr/api/records/1.0/search/?apikey=7c4e7563fc30cc228d982e4885d812cb7b22654d3539c190b016c972&dataset=mobilite-places-disponibles-parkings-en-temps-reel&rows=24');
         $content = $response->toArray();
         $infos = $content['records'];
         return $this->render('parking/index.html.twig', [
